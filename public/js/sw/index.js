6af827fc583e77c57098bd4f38ccc9031d2e28b4
Alberto Aragon Alvarez <ronin8503@gmail.com>
2017-08-03 23:36:20 -0500

    Caching files

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,14 +1,31 @@
-self.addEventListener("fetch", function(event) {
+let cacheName = 'wittr-static-v1'
+self.addEventListener('install', function(event) {
+  let urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff',
+  ]
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urlsToCache)
+    }),
+  )
+})
+
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request)
       .then(function(response) {
         if (response.status === 404) {
-          return fetch("/imgs/dr-evil.gif");
+          return fetch('/imgs/dr-evil.gif')
         }
-        return response;
+        return response
       })
       .catch(function() {
-        return new Response("Uh oh, that totally failed!");
-      })
-  );
-});
+        return new Response('Uh oh, that totally failed!')
+      }),
+  )
+})
